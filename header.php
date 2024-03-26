77b466463d3972898c603db1ae0322f85555b0f7
swapnilbhamare11 <sbhamare216@gmail.com>
2024-03-26 15:18:34 +0530

    Initial Commit

---
+++ header.php
@@ -0,0 +1,60 @@
+        <header class="main_header_area">
+            <div class="header_top">
+                <div class="container">
+                    <div class="header_top_inner">
+                        <div class="pull-left">
+                            <a href="tel:+919665819110"><i class="fa fa-phone"></i>+919665819110</a>
+                            <a href="mailto:npitsolutionsnashik@gmail.com"><i class="fa fa-envelope-o"></i>npitsolutionsnashik@gmail.com</a>
+                        </div>
+                        <div class="pull-right">
+                            <ul class="header_social">
+                                <li><a href="https://www.facebook.com/npitsolutions"><i class="fa fa-facebook"></i></a></li>                            
+                                <li><a href="https://www.instagram.com/npitsolutions/"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header_menu">
+                <nav class="navbar navbar-default">
+                    <div class="container">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">
+                                <img src="img/npit.png" alt="">
+                                <img src="img/npit.png" alt="">
+                            </a>
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                                <li class="">   <a href="index.php" >Home </a> </li>
+                             <li>   <a href="about-us.php" >About us </a> </li>
+                              <li>   <a href="rooms.php" >Room </a> </li>
+                               <li>   <a href="restaurant.php" >Restaurant </a> </li>
+                               <li>   <a href="lawns.php" >Lawns </a> </li>
+                              <li>   <a href="gallery.php" >Gallery </a> </li>
+                            
+                                <li><a href="shivneri-fort.php">Nearby tourist attractions </a></li>
+                                <li><a href="contact-us.php">Contact Us</a></li>
+                            </ul>
+                        <!--     <ul class="nav navbar-nav navbar-right">
+                                <li class="search_dropdown">
+                                    <a class="popup-with-zoom-anim" href="#test-search"><i class="icon icon-Search"></i></a>
+                                </li>
+                                <li class="book_btn">
+                                    <a class="book_now_btn" href="#">Book now</a>
+                                </li>
+                            </ul> -->
+                        </div><!-- /.navbar-collapse -->
+                    </div><!-- /.container-fluid -->
+                </nav>
+            </div>
+        </header>
